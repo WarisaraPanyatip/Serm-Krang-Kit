3e577175c732da36f6fd22da891ca1435c713d10
pincha45 <164159755+pincha45@users.noreply.github.com>
2024-07-13 20:36:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -144,3 +144,16 @@ dots.forEach((li, key) => {
     })
 })
 
+/* faq */
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faqs.forEach(item => {
+            if (item !== faq) {
+                item.classList.remove("active");
+            }
+        })
+        faq.classList.toggle("active");
+    })
+})
